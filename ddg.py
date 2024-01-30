b7c0d80976f1db74618eee8ad9850f986bb5b8d1
adlisovski <adlisovski@mts.ru>
2024-01-31 04:41:22 +0500

    Complete is it a bird or not lesson

--- ddg.py
+++ ddg.py
@@ -1,35 +1,63 @@
-# from duckduckgo_search import DDGS
-# from fastcore.all import *
+from duckduckgo_search import DDGS
+from fastcore.all import *
 
-# Just a comment
+def search_images(term, max_images=30):
+    print(f"Searching for '{term}'")
+    with DDGS() as ddgs:
+        # generator which yields dicts with:
+        # {'title','image','thumbnail','url','height','width','source'}
+        search_results = ddgs.images(keywords=term)       
+        # grap number of max_images urls
+        image_urls = [next(search_results).get("image") for _ in range(max_images)]
+        # convert to L (functionally extended list class from fastai)
+        return L(image_urls)
+
+urls = search_images('bird photos', max_images=1)
+urls[0]
+
+from fastdownload import download_url
+dest = 'bird.jpg'
+download_url(urls[0], dest, show_progress=False)
 
 from fastai.vision.all import *
+im = Image.open(dest)
+im.to_thumb(256,256)
 
-path = untar_data(URLs.PETS)/'images'
+download_url(search_images('forest photos', max_images=1)[0], 'forest.jpg', show_progress=False)
+Image.open('forest.jpg').to_thumb(256,256)
 
-def is_cat(x): return x[0].isupper() 
+searches = 'forest','bird'
+path = Path('bird_or_not')
+from time import sleep
 
-dls = ImageDataLoaders.from_name_func('.',
-    get_image_files(path), valid_pct=0.2, seed=42,
-    label_func=is_cat,
-    item_tfms=Resize(192))
+for o in searches:
+    dest = (path/o)
+    dest.mkdir(exist_ok=True, parents=True)
+    download_images(dest, urls=search_images(f'{o} photo'))
+    sleep(10)  # Pause between searches to avoid over-loading server
+    download_images(dest, urls=search_images(f'{o} sun photo'))
+    sleep(10)
+    download_images(dest, urls=search_images(f'{o} shade photo'))
+    sleep(10)
+    resize_images(path/o, max_size=400, dest=path/o)
+
+failed = verify_images(get_image_files(path))
+failed.map(Path.unlink)
+len(failed)
+
+dls = DataBlock(
+    blocks=(ImageBlock, CategoryBlock), 
+    get_items=get_image_files, 
+    splitter=RandomSplitter(valid_pct=0.2, seed=42),
+    get_y=parent_label,
+    item_tfms=[Resize(192, method='squish')]
+).dataloaders(path, bs=32)
+
+dls.show_batch(max_n=6)
 
 learn = vision_learner(dls, resnet18, metrics=error_rate)
 learn.fine_tune(3)
 
-learn.export('model.pkl')
-
-# def search_images(term, max_images=30):
-#     print(f"Searching for '{term}'")
-#     with DDGS() as ddgs:
-#         # generator which yields dicts with:
-#         # {'title','image','thumbnail','url','height','width','source'}
-#         search_results = ddgs.images(keywords=term)       
-#         # grap number of max_images urls
-#         image_urls = [next(search_results).get("image") for _ in range(max_images)]
-#         # convert to L (functionally extended list class from fastai)
-#         return L(image_urls)
-
-# # example usage:
-# urls = search_images("dog images", max_images=10)
-# print(urls[0])
+is_bird,_,probs = learn.predict(PILImage.create('bird.jpg'))
+print(f"This is a: {is_bird}.")
+print(f"Probability it's a bird: {probs[0]:.4f}")
